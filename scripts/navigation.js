d1de7b677eb9e4f51dd4cfcde78819e2b7ae34ca
MBarker2BYU <mbarker2@byupathway.edu>
2025-05-05 20:23:44 -0400

    Scrape #2

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,8 +1,18 @@
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const mobileMenu = document.getElementById('mobile-menu');
-const header = document.querySelector('header');
+const hamburgerIcon = document.querySelector('#hamburger-menu');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburgerIcon.addEventListener('click', ()=> 
-    {
+hamburgerIcon.addEventListener('click', () => {
+                
         hamburgerIcon.classList.toggle('open');
+        navMenu.classList.toggle('active');        
+    });
+
+// Close menu when clicking a link on mobile
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        if (window.innerWidth <= 600) {
+            hamburgerIcon.classList.remove('open');
+            navMenu.classList.remove('active');
+            }
+        });
     });
