24fdd38d39d72d179efb93a8f94f8f2ace460ff7
MBarker2BYU <mbarker2@byupathway.edu>
2025-05-05 20:26:50 -0400

    Starting over again

--- scripts/common.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-const currentyear = document.getElementById("currentyear");
-const lastmodified = document.getElementById("lastmodified");
-
-currentyear.textContent = "\u00A9" + today.getFullYear();
-lastmodified.textContent = document.lastModified;
