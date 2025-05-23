b6629d43f3d6b24d000ca3d44b4ec3e97f25847f
MBarker2BYU <mbarker2@byupathway.edu>
2025-05-23 08:43:31 -0400

    Weather Activity

---
+++ scripts/weather-api.js
@@ -0,0 +1,35 @@
+const apiKey = 'e76fca265a6ee2e92faa79e4ac4d0d6c';
+const latitude = 26.668174771096886 //49.751462456347355;
+const longitude = -81.98061166970946 //6.621305642992221;
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
